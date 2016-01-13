4f7776223528859c43f601ee55efdd4f0aa086c5
Will Dowling <will@autodeist.com>
2016-01-13 17:35:14 +0800

    Fixing nvidia detection

--- lib/facter/nvidia_detect.rb
+++ lib/facter/nvidia_detect.rb
@@ -1,6 +1,6 @@
 Facter.add(:nvidiagfx) do
 	confine :kernel => "Linux"
 	setcode do
-		Facter::Core::Execution.execute('lspci -d 10de: | grep VGA').present?
+		! Facter::Core::Execution.execute('lspci -d 10de: | grep VGA').blank?
 	end
 end
