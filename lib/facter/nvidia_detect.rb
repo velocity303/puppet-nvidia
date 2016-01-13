a7bb72d9ab5fcd130d0c9a9b10ae0d47bc0e528e
Will Dowling <will@autodeist.com>
2016-01-13 17:41:54 +0800

    Fixed nvidia detection

--- lib/facter/nvidia_detect.rb
+++ lib/facter/nvidia_detect.rb
@@ -1,6 +1,6 @@
 Facter.add(:nvidiagfx) do
 	confine :kernel => "Linux"
 	setcode do
-		Facter::Core::Execution.execute('lspci -d 10de: | grep VGA').to_s == ''
+		Facter::Core::Execution.execute('lspci -d 10de: | grep VGA').to_s != ''
 	end
 end
