28c44544f566f2379a04eae5573661bcae5db322
James E. Jones <velocity303@gmail.com>
2016-07-13 00:10:00 -0500

    dont use ppa so that it doenst refresh every run

--- manifests/linux/ubuntu.pp
+++ manifests/linux/ubuntu.pp
@@ -8,10 +8,15 @@ class nvidia::linux::ubuntu (
   ensure_resource('package', 'pciutils', {'ensure' => 'present' })
 
   if $::nvidiagfx {
-    apt::ppa { 'ppa:graphics-drivers/ppa':
-      package_manage => true
+    apt::key { 'graphics-drivers':
+      id     => '2388FF3BE10A76F638F80723FCAE110B1118213C',
+      source => 'keyserver.ubuntu.com',
     }
-
+    apt::source { 'graphics-drivers':
+      location => 'http://ppa.launchpad.net/graphics-drivers/ppa/ubuntu',
+      release  => $::lsbdistcodename,
+      repos    => 'main',
+    }
     apt::pin { 'main':
       packages => 'nvidia-*',
       priority => 501,
